f24461fe93a51be86e2774c6307f48d5be388fc2
tanque <paoletti.francesco@gmail.com>
2024-11-05 20:21:33 +0100

    add custom task user_report

--- src/app.rs
+++ src/app.rs
@@ -57,6 +57,7 @@ impl Hooks for App {
     }
 
     fn register_tasks(tasks: &mut Tasks) {
+        tasks.register(tasks::user_report::UserReport);
         tasks.register(tasks::seed::SeedData);
     }
 
